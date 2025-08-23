73a858d3694d7c4ff27cd6f3a7ac5d7d2698d4b5
lucasnovais01 <novais.oliveira@aluno.ifsp.edu.br>
2025-08-22 22:25:01 -0300

    serviceRemoverincopleto

--- src/cidade/service/cidade.service.findone.ts
+++ src/cidade/service/cidade.service.findone.ts
@@ -3,4 +3,5 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class CidadeServiceFindOne {
   constructor() {}
+  //Está incompleto, falta este e o findall, verificar o codigo no Moodle até domingo o professor coloca
 }
